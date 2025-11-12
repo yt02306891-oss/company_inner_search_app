455c1a5bef2542754555b0355eac472916601aba
yt02306891-oss <yt02306891@gmail.com>
2025-11-12 18:10:37 +0900

    modify

--- components.py
+++ components.py
@@ -26,10 +26,10 @@ def display_select_mode():
     回答モードのラジオボタンを表示
     """
     # 回答モードを選択する用のラジオボタンを表示
-    col1, col2 = st.columns([100, 1])
+    col1, col2 = st.sidebarcolumns([100, 1])
     with col1:
         # 「label_visibility="collapsed"」とすることで、ラジオボタンを非表示にする
-        st.sidebar.session_state.mode = st.radio(
+        st.sidebar.session_state.mode = st.sidebar.radio(
             label="",
             options=[ct.ANSWER_MODE_1, ct.ANSWER_MODE_2],
             label_visibility="collapsed"
