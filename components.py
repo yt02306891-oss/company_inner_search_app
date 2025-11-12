d6f0130374a47e9a49eb07ff0e4d676fe7e098b9
yt02306891-oss <yt02306891@gmail.com>
2025-11-12 18:46:31 +0900

    modify

--- components.py
+++ components.py
@@ -48,22 +48,21 @@ def display_initial_ai_message():
     """
     AIメッセージの初期表示
     """
-    # 「st.success()」とすると緑枠で表示される
-    st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
-    
-    with st.sidebar.chat_message("assistant"):
-        # 「社内文書検索」の機能説明
-        st.sidebar.markdown("**【「社内文書検索」を選択した場合】**")
-        # 「st.info()」を使うと青枠で表示される
-        st.sidebar.info("入力内容と関連性が高い社内文書のありかを検索できます。")
-        # 「st.code()」を使うとコードブロックの装飾で表示される
-        # 「wrap_lines=True」で折り返し設定、「language=None」で非装飾とする
-        st.sidebar.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
 
-        # 「社内問い合わせ」の機能説明
-        st.sidebar.markdown("**【「社内問い合わせ」を選択した場合】**")
-        st.sidebar.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
-        st.sidebar.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
+    # メインエリア（色付きの案内を出したいなら）
+    st.success("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。サイドバーで利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+    st.warning("具体的に入力したほうが期待通りの回答を得やすいです。")
+
+    # サイドバー
+    with st.sidebar:
+        st.markdown("**【「社内文書検索」を選択した場合】**")
+        st.info("入力内容と関連性が高い社内文書のありかを検索できます。")
+        st.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
+
+        st.markdown("**【「社内問い合わせ」を選択した場合】**")
+        st.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
+        st.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
+
 
 
 def display_conversation_log():
